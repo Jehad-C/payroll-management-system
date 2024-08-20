2540d5a2c2a1e7fe6b5fe30393790f13737cb67b
Jehad Cemanes <jehadcemanesece@gmail.com>
2024-08-21 01:02:31 +0800

    Remove employee list service

--- 02-frontend/payroll-management-system/src/app/app.routes.ts
+++ 02-frontend/payroll-management-system/src/app/app.routes.ts
@@ -1,14 +1,12 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegistrationComponent } from './components/registration/registration.component';
-import { EmployeeListComponent } from './components/employee-list/employee-list.component';
 import { EmployeeComponent } from './components/employee/employee.component';
 import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'registration', component: RegistrationComponent },
-    { path: 'employees', component: EmployeeListComponent },
-    { path: 'employee/id/:id', component: EmployeeComponent },
-    { path: 'home', component: HomeComponent }
+    { path: 'home', component: HomeComponent },
+    { path: 'employee/id/:id', component: EmployeeComponent }
 ];
